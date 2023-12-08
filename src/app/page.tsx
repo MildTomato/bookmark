4b24d81a5991a021d538025c4cf7dec0bbaaa88a
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-12-08 14:36:57 +0800

    empty state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,7 @@ import { BookmarkInsert } from "./BookmarkInsert";
 export default function Home() {
   return (
     <div className="max-w-site px-site mx-auto">
-      <div className="flex flex-row gap-3 w-full items-center">
-        <Label className="relative grow">
-          <Search className="absolute left-4 top-2 w-4" />
-          <Input
-            name="url"
-            placeholder="Search for a website"
-            type="url"
-            className="pl-12"
-          />
-        </Label>
-        <div className="h-[2rem] w-px bg-border"></div>
-        <BookmarkInsert />
-      </div>
-      <div className="py-4">
-        <Websites />
-      </div>
+      <Websites />
     </div>
   );
 }
